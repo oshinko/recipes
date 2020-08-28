46a24af25b46c9fd3b6289910649c7b988e313e9
Oshinko <osnk@renjaku.jp>
2020-08-29 02:55:34 +0900

    Update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def pick
+    @picked = Article.order(Arel.sql('RANDOM()')).first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
+  get '/pick', to: 'welcome#pick'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles do
     resources :comments
   end
-  root 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 end
