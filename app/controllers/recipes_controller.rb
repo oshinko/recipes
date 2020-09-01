2cf8fb88f8a0ac51b5a5e654e21579d3267c6bbf
Oshinko <osnk@renjaku.jp>
2020-09-01 18:41:21 +0900

    Update recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,15 +12,20 @@ class RecipesController < ApplicationController
   end
 
   def create
-    view_context.current_user.recipes.create(recipe_params)
-    redirect_to user_path(view_context.current_user)
+    @recipe = view_context.current_user.recipes.create(recipe_params)
+  
+    if @recipe.save
+      redirect_to user_path(view_context.current_user)
+    else
+      render 'new'
+    end
   end
 
   def update
     @recipe = view_context.current_user.recipes.find(params[:id])
   
     if @recipe.update(recipe_params)
-      redirect_to @recipe
+      redirect_to user_path(view_context.current_user)
     else
       render 'edit'
     end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: { minimum: 1 }
 end
