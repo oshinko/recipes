6710d6e0f034dafaf75ffd42027f87339d53389d
Oshinko <osnk@renjaku.jp>
2020-09-04 16:12:53 +0900

    Add support for secure URL for user images

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.credentials.twitter[:api_key],
-                     Rails.application.credentials.twitter[:api_secret]
+  provider :twitter,
+    Rails.application.credentials.twitter[:api_key],
+    Rails.application.credentials.twitter[:api_secret],
+    {
+      :secure_image_url => 'true'
+    }
 end
