841eae342a2ac4cbf0588498b7b8017455910630
Oshinko <osnk@renjaku.jp>
2020-09-05 09:41:53 +0900

    Add .local to config.hosts

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Recipicker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.hosts << '.local'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
